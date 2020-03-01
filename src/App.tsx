d9d18d54f1ec7f6307371828b0dcd8dedeeea5f5
tebracb <bori.temesi@gmail.com>
2020-03-01 16:58:22 +0100

    set up project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as d3 from 'd3';
+import Itemlist from './Components/Itemlist';
+import { Item } from './Components/Item';
+import ItemChart from './Components/ItemChart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppState {
+  items: Item[];
+  selectedItem: any;
 }
 
+class App extends React.Component<{}, AppState> {
+  readonly state = {
+    items: [],
+    selectedItem: {}
+  };
+
+  setSelectedItem = (item: any) => {
+    this.setState({
+      selectedItem: item
+    });
+  };
+
+  componentDidMount() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+      .then((results: Response) => {
+        return results.json();
+      })
+      .then((results: any) => {
+        console.log(results);
+        this.setState({
+          items: results.drinks
+        });
+      })
+      .catch((error: any) => {
+        console.log('error: ' + error);
+      });
+
+    d3.selectAll('h1').style('color', 'blue');
+
+    let scale = d3
+      .scaleLinear()
+      .domain([0, 1600])
+      .range([0, 1600]);
+
+    let xAxis: any = d3.axisBottom(scale);
+    d3.select('#xAxis').call(xAxis);
+
+    let yAxis: any = d3.axisLeft(scale);
+    d3.select('#yAxis').call(yAxis);
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hoi</h1>
+        <Itemlist
+          items={this.state.items}
+          setSelectedItem={this.setSelectedItem}
+          selectedItem={this.state.selectedItem}
+        />
+        <ItemChart selectedItem={this.state.selectedItem} />
+      </div>
+    );
+  }
+}
 export default App;
